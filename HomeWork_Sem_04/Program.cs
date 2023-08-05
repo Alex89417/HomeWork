39683be730f20f15d79945e03bb370757fc28093
Alex89417 <zezylia25@gmail.com>
2023-08-05 20:39:43 +0300

    add 3 tasks

---
+++ HomeWork_Sem_04/Program.cs
@@ -0,0 +1,77 @@
+﻿
+/* Task 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B:
+
+void numVDegree(int num, int degree)
+{
+    int res = num;
+    for(int count = 1; count < degree; count++)
+        res *= num;
+   
+    Console.WriteLine($"{num}, {degree} -> {res}");
+}
+
+Repeat:
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a degree: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+if (num <= 0 || deg <= 0) 
+{
+    Console.Write("Uncorrect data! --> ");
+    goto Repeat;
+}
+
+else numVDegree(num, deg);
+*/
+
+
+
+/* Task 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе:
+
+void SumInNumbers(int number)
+{
+    int sum = 0;
+    int num = number;
+    while(num >= 10)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    sum += num;
+    Console.WriteLine($"{number} -> {sum}");
+}
+
+Repeat:
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0) 
+{
+    Console.Write("Uncorrect data! --> ");
+    goto Repeat;
+}
+
+else SumInNumbers(num);
+*/
+
+
+
+//Task 29. Напишите программу, которая задаёт массив из m элементов и выводит их на экран:
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input the {i} index value: ");
+        array[i] = Convert.ToInt32(Console.Read());
+    }
+    return array;
+}   
+
+Console.Write("input a length of an array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray(length);
+Console.Write(myArray);
